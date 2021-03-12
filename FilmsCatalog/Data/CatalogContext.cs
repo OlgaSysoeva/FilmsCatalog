5b7b3e5118b4d67df2e0f70d78b3db1842587ff5
Olga Sysoeva <sysoevaolya@gmail.com>
2021-03-12 21:20:52 +0300

    Добав. сущность и настроено подкл. к базе (#1)

---
+++ FilmsCatalog/Data/CatalogContext.cs
@@ -0,0 +1,33 @@
+﻿using FilmsCatalog.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace FilmsCatalog.Data
+{
+    public class CatalogContext : DbContext
+    {
+        public CatalogContext(DbContextOptions options)
+            : base(options) { }
+
+        public DbSet<Film> Films { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Film>()
+                .HasKey(f => f.Id);
+
+            modelBuilder.Entity<Film>()
+                .Property(f => f.Name)
+                .IsRequired();
+
+            modelBuilder.Entity<Film>()
+                .Property(f => f.Producer)
+                .IsRequired();
+
+            modelBuilder.Entity<Film>()
+                .Property(f => f.Poster)
+                .IsRequired();
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ FilmsCatalog/Migrations/Catalog/20210312161144_InitFilmCatalog.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FilmsCatalog.Migrations.Catalog
+{
+    public partial class InitFilmCatalog : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Films",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    IssueYear = table.Column<int>(type: "int", nullable: false),
+                    Producer = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    UserId = table.Column<int>(type: "int", nullable: false),
+                    Poster = table.Column<byte[]>(type: "varbinary(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Films", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Films");
+        }
+    }
+}
