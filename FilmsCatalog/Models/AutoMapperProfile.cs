e2de1bf6c0536ed90bb5aaf6f3940a2b5f526982
Olga Sysoeva <sysoevaolya@gmail.com>
2021-03-12 21:44:23 +0300

    Добав. модель и automapper (#3)

---
+++ FilmsCatalog/Models/AutoMapperProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using FilmsCatalog.Entities;
+
+namespace FilmsCatalog.Models
+{
+    /// <summary>
+    /// Карта конвертации моделей.
+    /// </summary>
+    public class AutoMapperProfile : Profile
+    {
+        public AutoMapperProfile()
+        {
+            CreateMap<Film, FilmViewModel>().ReverseMap();
+        }
+    }
+}
