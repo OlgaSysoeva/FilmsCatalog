046ce66ee75e0ac0147d813a9f4523e019d22ce2
Olga Sysoeva <sysoevaolya@gmail.com>
2021-03-14 14:16:47 +0300

    Добавлено логирование (#7)

--- FilmsCatalog/Controllers/CatalogController.cs
+++ FilmsCatalog/Controllers/CatalogController.cs
@@ -2,10 +2,8 @@
 using FilmsCatalog.Entities;
 using FilmsCatalog.Interfaces;
 using FilmsCatalog.Models;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
@@ -14,18 +12,15 @@ namespace FilmsCatalog.Controllers
     public class CatalogController : Controller
     {
         private readonly IDatabaseRepository<Film> _filmRepository;
-        private readonly ILogger<FilmController> _logger;
         private readonly IMapper _mapper;
         private readonly SignInManager<User> _signInManager;
 
         public CatalogController(
             IDatabaseRepository<Film> filmRepository,
-            ILogger<FilmController> logger,
             IMapper mapper,
             SignInManager<User> signInManager)
         {
             _filmRepository = filmRepository;
-            _logger = logger;
             _mapper = mapper;
             _signInManager = signInManager;
         }
--- FilmsCatalog/Program.cs
+++ FilmsCatalog/Program.cs
@@ -1,11 +1,8 @@
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace FilmsCatalog
 {
@@ -13,11 +10,29 @@ namespace FilmsCatalog
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+            var logger = host.Services.GetRequiredService<ILogger<Program>>();
+
+            try
+            {
+                logger.LogInformation("Start FilmCatalog");
+                host.Run();
+                logger.LogInformation("Stopped FilmCatalog");
+            }
+            catch (Exception exception)
+            {
+                logger.LogError(exception, "Stopped FilmCatalog because of exception");
+                throw;
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .ConfigureLogging(logging =>
+                {
+                    logging.ClearProviders();
+                    logging.AddConsole();
+                })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
