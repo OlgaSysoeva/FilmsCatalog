31ba4b2e53837e549b8c206b24b68667a2c7b8ea
Olga Sysoeva <sysoevaolya@gmail.com>
2021-03-16 15:33:55 +0300

    Настройка парам. конфигурации хранения файлов (#7)

---
+++ FilmsCatalog/Models/FilesConfigModel.cs
@@ -0,0 +1,19 @@
+﻿
+namespace FilmsCatalog.Models
+{
+    /// <summary>
+    /// Конфигурация настроек хранения файлов.
+    /// </summary>
+    public class FilesConfigModel
+    {
+        /// <summary>
+        /// Путь к постеру по умолчанию. Используетсяесть постер не задан.
+        /// </summary>
+        public string DefaultFilePath { get; set; }
+
+        /// <summary>
+        /// Путь хранения файлов.
+        /// </summary>
+        public string SavePath { get; set; }
+    }
+}
